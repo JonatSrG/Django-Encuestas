8535ea3e0bb6c94d4ac80b4d9009c9f1e002a245
JonatSrG <JonatSrG>
2022-08-02 20:42:35 -0500

    Creando Test y Corrigiendo Test

--- premiosapp/polls/tests.py
+++ premiosapp/polls/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+import datetime
+
+# Model or View
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(day=30)
+        future_question = Question(question_text="¿?", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
